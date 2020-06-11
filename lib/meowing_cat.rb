8e05e96954a4465b4ce4952b1554a4d26ededd12
Rumiko Acopa Rios <rumikorios@gmail.com>
2020-06-11 01:46:18 +0000

    Automatically backed up by Learn

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,13 @@
-## code your solution here. 
+Class Cat
+def name=(cat_name)
+  @this_cats_name = cat_name
+end
+
+def name
+  @this_cats_name
+end
+end
+  maru = Cat.new
+  maru.name = "maru"
+
+  puts maru.name# let(:maru) {Cat.new}# code your solution here. 
